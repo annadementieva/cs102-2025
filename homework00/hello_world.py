3d14e8787ae303a62308f19695bb82a93b8ecf7e
annadementieva <demanna0709@gmail.com>
2025-12-01 11:57:19 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,8 @@
-def text():
+"Основная функция программы, которая в конце выводит приветственное сообщение."
+def main():
+    "Модуль hello_world, выводит приветственное сообщение."
+    message = "Hello world!"
     return message
+
+if __name__ == "__main__":
+    main()
